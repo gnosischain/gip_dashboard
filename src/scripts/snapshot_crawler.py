e0bd12a50bae738871320dd0f1d451ce6fc748bc
hdser <hugo.serodio@gnosis.io>
2025-10-02 09:57:24 +0200

    update crawler and github action

--- src/scripts/snapshot_crawler.py
+++ src/scripts/snapshot_crawler.py
@@ -108,7 +108,9 @@ def extract_funding_info(text):
         }
 
 def fetch_html_content(url):
-    header = {'User-Agent': 'Mozilla/5.0'}
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
     response = requests.get(url, headers=header)
     if response.status_code == 200:
         return response.text
